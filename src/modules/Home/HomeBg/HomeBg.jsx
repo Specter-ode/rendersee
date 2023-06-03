ca9babadea9c7a1ba90451c6a0c3670b1d1add66
unknown <specter.ode@gmail.com>
2023-06-03 16:23:52 +0300

    upgrade

--- src/modules/Home/HomeBg/HomeBg.jsx
+++ src/modules/Home/HomeBg/HomeBg.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import s from './HomeBg.module.css';
 
-import ImgBefore from '../../../assets/images/before.jpg';
-import ImgAfter from '../../../assets/images/after.jpg';
+import ImgBefore from '../../../assets/images/before.png';
+import ImgAfter from '../../../assets/images/after.png';
 
 const HomeBg = () => {
   const [leftContainerWidth, setLeftContainerWidth] = useState(75);
@@ -10,12 +10,10 @@ const HomeBg = () => {
 
 
   useEffect(() => {
-    console.log('start: ', start);
     let interval;
     if (start) {
       setLeftContainerWidth(25);
       interval = setInterval(() => {
-        console.log('========выполнился интервал');
         setLeftContainerWidth(prevWidth => (prevWidth === 75 ? 25 : 75));
       }, 16000);
     }
@@ -28,10 +26,10 @@ const HomeBg = () => {
   return (
     <div className={s.container} onMouseEnter={() => setStart(true)}>
       <div className={s.leftContainer} style={{ width: `${leftContainerWidth}vw` }}>
-        <img className={s.leftImage} src={ImgBefore} alt="Interior before" width={880} height={820} />
+        <img className={s.leftImage} src={ImgBefore} alt="Interior before" width={1680} height={810}/>
       </div>
       <div className={s.rightContainer}>
-        <img className={s.rightImage} src={ImgAfter} alt="Interior after" width={880} height={820} />
+        <img className={s.rightImage} src={ImgAfter} alt="Interior after" width={1680} height={810} />
       </div>
     </div>
   );
