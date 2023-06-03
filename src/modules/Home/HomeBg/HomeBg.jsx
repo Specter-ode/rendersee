3ede824807ff75d552d6d44a4bf73adc9c04f7ec
unknown <specter.ode@gmail.com>
2023-06-03 14:42:17 +0300

    start move after hover

--- src/modules/Home/HomeBg/HomeBg.jsx
+++ src/modules/Home/HomeBg/HomeBg.jsx
@@ -5,22 +5,28 @@ import ImgBefore from '../../../assets/images/before.jpg';
 import ImgAfter from '../../../assets/images/after.jpg';
 
 const HomeBg = () => {
-  const [leftContainerWidth, setLeftContainerWidth] = useState(80);
+  const [leftContainerWidth, setLeftContainerWidth] = useState(75);
+  const [start, setStart] = useState(false);
+
 
   useEffect(() => {
-    setLeftContainerWidth(20);
-  }, []);
+    console.log('start: ', start);
+    let interval;
+    if (start) {
+      setLeftContainerWidth(25);
+      interval = setInterval(() => {
+        console.log('========выполнился интервал');
+        setLeftContainerWidth(prevWidth => (prevWidth === 75 ? 25 : 75));
+      }, 16000);
+    }
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setLeftContainerWidth(prevWidth => (prevWidth === 80 ? 20 : 80));
-    }, 15000);
-
-    return () => clearInterval(interval);
-  }, [leftContainerWidth]);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [start]);
 
   return (
-    <div className={s.container}>
+    <div className={s.container} onMouseEnter={() => setStart(true)}>
       <div className={s.leftContainer} style={{ width: `${leftContainerWidth}vw` }}>
         <img className={s.leftImage} src={ImgBefore} alt="Interior before" width={880} height={820} />
       </div>
